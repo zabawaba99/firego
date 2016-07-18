17cae3f7e0b371efdc8b0f7bc0b629783c62b727
Satyam Surendra <satyam.surendra@ibibogroup.com>
2016-07-18 19:08:18 +0530

    new files for goibibo firego fork

---
+++ auth.go
@@ -0,0 +1,11 @@
+package firego
+
+// Auth sets the custom Firebase token used to authenticate to Firebase.
+func (fb *Firebase) Auth(token string) {
+	fb.params.Set(authParam, token)
+}
+
+// Unauth removes the current token being used to authenticate to Firebase.
+func (fb *Firebase) Unauth() {
+	fb.params.Del(authParam)
+}
---
+++ push_test.go
@@ -0,0 +1,27 @@
+package firego
+
+import (
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/zabawaba99/firetest"
+)
+
+func TestPush(t *testing.T) {
+	t.Parallel()
+	var (
+		payload = map[string]interface{}{"foo": "bar"}
+		server  = firetest.New()
+	)
+	server.Start()
+	defer server.Close()
+
+	fb := New(server.URL, nil)
+	childRef, err := fb.Push(payload)
+	assert.NoError(t, err)
+
+	path := strings.TrimPrefix(childRef.String(), server.URL+"/")
+	v := server.Get(path)
+	assert.Equal(t, payload, v)
+}
