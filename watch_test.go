47bbe7a8d079b624a497a945721eaf414df02ec4
Yuki Ito <acomagu@gmail.com>
2018-06-13 23:45:57 +0900

    Make public firetest package
    Because it's useful for other packages using firego to test

--- watch_test.go
+++ watch_test.go
@@ -10,7 +10,7 @@ import (
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
-	"github.com/zabawaba99/firego/internal/firetest"
+	"github.com/zabawaba99/firego/firetest"
 )
 
 func setupLargeResult() string {
